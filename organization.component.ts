26d9072696d73c5cf7ca4afc3354c5c178fc0f71
CoderGeek <shahajipatil22@gmail.com>
2022-01-19 16:47:46 +0530

    Update organization.component.ts

--- organization.component.ts
+++ organization.component.ts
@@ -13,6 +13,25 @@ export class OrganizationComponent implements OnInit {
 
   ngOnInit() {
     this.navigateAuthenticatedUser();
+    
+    var theThing = null;
+var replaceThing = function () {
+  var priorThing = theThing;  // hold on to the prior thing
+  var unused = function () {
+    // 'unused' is the only place where 'priorThing' is referenced,
+    // but 'unused' never gets invoked
+    if (priorThing) {
+      console.log("hi");
+    }
+  };
+  theThing = {
+    longStr: new Array(1000000).join('*'),  // create a 1MB object
+    someMethod: function () {
+      console.log(someMessage);
+    }
+  };
+};
+setInterval(replaceThing, 1000);    // invoke `replaceThing' once every second
   }
 
   async function init(args, options) {
